8d2889d4fd032d713cc33619a8bd2a9b85025b8b
Aayush2425 <122793682+Aayush2425@users.noreply.github.com>
2026-01-07 18:46:54 +0530

    updated the url

--- frontend/IMS_WEB/src/environments/environment.ts
+++ frontend/IMS_WEB/src/environments/environment.ts
@@ -2,5 +2,6 @@ export const environment = {
   production: true,
   // baseUrl: 'https://1j1t9nm6-5000.inc1.devtunnels.ms/api',
   // baseUrl: 'http://localhost:5134/api',
-  baseUrl: 'http://localhost:5001/api',
+  // baseUrl: 'http://localhost:5001/api',
+  baseUrl: 'https://ims-backend-api.azurewebsites.net/api',
 };
