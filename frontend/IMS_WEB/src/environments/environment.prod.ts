0b39a14bb93afa52c7c150ec8c77135384b07ab2
Aayush2425 <122793682+Aayush2425@users.noreply.github.com>
2026-01-07 18:05:26 +0530

    Angular frontend

--- frontend/IMS_WEB/src/environments/environment.prod.ts
+++ frontend/IMS_WEB/src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiBaseUrl: 'https://imsbackend.azurewebsites.net/api',
+  apiBaseUrl: 'https://ims-backend-api.azurewebsites.net/api',
 };
--- frontend/IMS_WEB/src/environments/environment.ts
+++ frontend/IMS_WEB/src/environments/environment.ts
@@ -1,5 +1,6 @@
 export const environment = {
   production: true,
-  baseUrl: 'https://1j1t9nm6-5000.inc1.devtunnels.ms/api',
+  // baseUrl: 'https://1j1t9nm6-5000.inc1.devtunnels.ms/api',
   // baseUrl: 'http://localhost:5134/api',
+  baseUrl: 'http://localhost:5001/api',
 };
